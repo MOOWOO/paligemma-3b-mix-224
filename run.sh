044b6c8406741407652e0a396bf58192d7baca0f
kgmin <cdc.asic@gmail.com>
2024-07-28 23:22:03 +0900

    api key add

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source paligemma-env/bin/activate 
+# source paligemma-env/bin/activate 
 
 # Run the FastAPI application with uvicorn
 uvicorn main:app --host 0.0.0.0 --port 8000 --log-config logging_config.py
