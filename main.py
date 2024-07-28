75e1b8bb47ef20f586a8e0eeaf9104e653dbcc82
kgmin <cdc.asic@gmail.com>
2024-07-28 23:18:17 +0900

    api key add

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import logging
 import os
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, Header, Depends
 from paligemma import PaliGemma
+from dotenv import load_dotenv
 from logging_config import logger  # Import the logger
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the API key from the environment variable
+API_KEY = os.getenv("API_KEY")
 
 # Ensure the uploads directory exists
 UPLOAD_DIRECTORY = "uploads"
@@ -11,8 +18,14 @@ os.makedirs(UPLOAD_DIRECTORY, exist_ok=True)
 app = FastAPI()
 model = PaliGemma()
 
+# Dependency to check for API key in the request header
+def verify_api_key(x_api_key: str = Header(...)):
+    if x_api_key != API_KEY:
+        logger.error("Invalid API Key")
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+
 @app.post("/generate")
-async def generate(task: str, image: UploadFile = File(...)):
+async def generate(task: str, image: UploadFile = File(...), api_key: str = Depends(verify_api_key)):
     try:
         logger.debug(f"Received request with task: {task}")
         
@@ -33,10 +46,9 @@ async def generate(task: str, image: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 @app.post("/predict")
-@app.post("/predict")
-async def generate(task: str, image: str):
+async def predict(task: str, image: str, api_key: str = Depends(verify_api_key)):
     try:
-        logger.debug(f"Received request with task: {task}")
+        logger.debug(f"Received request with task: {task} and image URL: {image}")
 
         # Run the model
         output = model.run(task, image)
@@ -47,7 +59,6 @@ async def generate(task: str, image: str):
         logger.error(f"Error occurred: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
-빠졌다
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
